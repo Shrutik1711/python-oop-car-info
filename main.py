a97684a8d4e4671317e5dbe9f46ea3cb78eae774
Shrutik Nandeshwar <123307004+Shrutik1711@users.noreply.github.com>
2026-01-08 19:40:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+class Car :
+    def __init__(self,name,brand, model,price,color,):
+        self.name = name
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.color = color
+        
+    def car_info(self):
+        print(car1.name)
+        print(car1.brand)
+        print(car1.model)
+        print(car1.price)
+        print(car1.color)
+        
+        
+        
+car1 = Car("TATA NEXON EV", "TATA" , "EV - 2024" , 145200, "Navy Blue")
+car1.car_info()
+    
